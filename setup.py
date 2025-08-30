dc67d047e9a069c361f4343d9e5ff7319e9903d5
Diego Hernandez <dahernandezsilvestre@gmail.com>
2025-08-29 18:30:15 -0600

    feat: test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (this_dir / "README.md").read_text(encoding="utf-8")
 # testingss2jsjs
 setup(
     name="titanic-ml-pipeline",
-    version="0.1.3",
+    version="0.1.5",
     description="Pipeline Titanic con scikit-learn (SVD + LogisticRegression)",
     author="Diego Hernandez",
     packages=find_packages(),
